597188a2f543d0b1af86f0234dea8f1a22a60247
hazn <aitakprojects@gmail.com>
2022-09-01 19:12:45 +0200

    redesign `index.tsx`

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,26 +1,43 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
+import { Head } from "$fresh/runtime.ts";
+import { Section } from "../components/Section.tsx";
 
 export default function Home() {
   return (
-    <html class={tw`md:text-2xl`}>
-      <head>
+    <html class={tw`md:text-xl font-serif`}>
+      <Head>
         <meta name="viewport" content="width=device-width" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <title>Hasan Atak's Homepage</title>
-      </head>
+      </Head>
       <body>
-        <div class={tw`bg-white max-w-4xl min-h-screen mx-auto`}>
-          <img
-            class={tw`h-20 mx-auto pt-4`}
-            src="/hazn_logo.svg"
-            alt="logo saying hazn in fancy way"
-          />
-          <p class={tw`my-6`}>
-            Welcome to `fresh`. Try updating this message in the
-            ./routes/index.tsx file, and refresh.
-          </p>
+        <div
+          class={tw`max-w-3xl min-h-screen mx-auto bg-white md:border-x-sm border-black`}
+        >
+          {/* header */}
+          <header class={tw`relative`}>
+            <div class={tw`flex h-[2.95rem] border-b-md border-black`}>
+              <div class={tw`w-[33%]`}></div>
+              {/* hazn logo */}
+              <img
+                class={tw`flex-none h-24`}
+                src="/hazn_logo.svg"
+                alt="logo saying hazn in fancy way"
+              />
+              <div class={tw`flex-none place-self-center pr-4`}>
+                as above, so below
+              </div>
+            </div>
+          </header>
+
+          <main class={tw`px-4`}>
+            <Section>
+              <div class={tw`h-[40vh]`}></div>
+            </Section>
+            <Section heading="Projects"></Section>
+          </main>
         </div>
       </body>
     </html>
