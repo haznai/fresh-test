fde0bd2a66870af38a539442f541e516667e9889
hazn <aitakprojects@gmail.com>
2022-09-01 19:12:34 +0200

    add `Section` component

---
+++ components/Section.tsx
@@ -0,0 +1,24 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+import type { VNode } from "preact";
+
+interface SectionProps {
+  heading?: string;
+  children?: VNode[] | VNode;
+}
+
+export function Section({ heading, children }: SectionProps) {
+  return (
+    <section>
+      {heading != undefined ? (
+        <div>
+          <h1 class={tw`text-3xl font-semibold`}>{heading}</h1>
+          <div class={tw`border-b-sm border-black -mx-4`}></div>
+        </div>
+      ) : null}
+
+      {children}
+    </section>
+  );
+}
