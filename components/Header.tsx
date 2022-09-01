c4f61e3b8495480bdd37961fbc6dd2c67c6cd8b9
hazn <aitakprojects@gmail.com>
2022-09-02 00:11:04 +0200

    refactor `<header>` into `Header` component

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+
+export function Header() {
+  return (
+    <header class={tw`relative`}>
+      <div class={tw`flex h-[2.95rem] border-b-md border-black`}>
+        <div class={tw`w-[33%]`}></div>
+        {/* hazn logo */}
+        <img
+          class={tw`flex-none h-24`}
+          src="/hazn_logo.svg"
+          alt="logo saying hazn in fancy way"
+        />
+        {/* as above, so below */}
+        <div
+          class={tw`inline-flex flex-none place-self-center justify-self-right pr-4 -ml-8 font-serif`}
+        >
+          <span class={tw`place-self-center`}>as above, so below</span>
+          <span>
+            <img class={tw`h-6 place-self-center`} src="/sparkles.svg"></img>
+          </span>
+        </div>
+      </div>
+    </header>
+  );
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -3,6 +3,7 @@ import { h } from "preact";
 import { tw } from "@twind";
 import { Head } from "$fresh/runtime.ts";
 import { Section } from "../components/Section.tsx";
+import { Header } from "../components/Header.tsx";
 
 export default function Home() {
   return (
@@ -14,25 +15,9 @@ export default function Home() {
       </Head>
       <body>
         <div
-          class={tw`max-w-3xl min-h-screen mx-auto bg-white md:border-x-sm border-black`}
+          class={tw`max-w-xl min-h-screen mx-auto bg-white md:border-x-sm border-black`}
         >
-          {/* header */}
-          <header class={tw`relative`}>
-            <div class={tw`flex h-[2.95rem] border-b-md border-black`}>
-              <div class={tw`w-[33%]`}></div>
-              {/* hazn logo */}
-              <img
-                class={tw`flex-none h-24`}
-                src="/hazn_logo.svg"
-                alt="logo saying hazn in fancy way"
-              />
-              {/* as above, so below */}
-              <div class={tw`flex-none place-self-center pr-4 font-serif`}>
-                as above, so below
-              </div>
-            </div>
-          </header>
-
+          <Header />
           <main class={tw`px-4`}>
             {/* about me */}
             <Section>
