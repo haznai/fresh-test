fa7c2b5b47b02e417cd02716b34c07ac76d8b370
hazn <aitakprojects@gmail.com>
2022-09-01 22:07:56 +0200

    configure fonts

--- utils/twind.ts
+++ utils/twind.ts
@@ -2,7 +2,26 @@ import { IS_BROWSER } from "$fresh/runtime.ts";
 import { Configuration, setup } from "twind";
 export * from "twind";
 export const config: Configuration = {
-  preflight: true,
+  preflight: {
+    // custom fonts
+    "@font-face": [
+      {
+        fontFamily: "Source\\ Serif\\ 4",
+        fontWeight: 400,
+        src: "url(/fonts/source-serif-4-v7-latin-regular.woff) format(woff)",
+      },
+      {
+        fontFamily: "Source\\ Serif\\ 4",
+        fontWeight: 600,
+        src: "url(/fonts/source-serif-4-v7-latin-600.woff) format(woff)",
+      },
+      {
+        fontFamily: "Source\\ Sans\\ 3",
+        fontWeight: 400,
+        src: "url(/fonts/source-sans-3-v8-latin-regular.woff) format(woff)",
+      },
+    ],
+  },
   darkMode: "class",
   mode: "strict",
   theme: {
@@ -10,6 +29,11 @@ export const config: Configuration = {
       sm: ".0625rem",
       md: "0.125rem",
       lg: "0.2rem",
+    },
+    fontFamily: {
+      sans: "Source\\ Sans\\ 3, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+      serif:
+        "Source\\ Serif\\ 4, ui-serif, Georgia, Cambria, 'Times New Roman', Times, serif",
     },
   },
 };
