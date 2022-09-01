7ce19f7fb49c2d7e23ee9b81afa46526926fcd5f
hazn <aitakprojects@gmail.com>
2022-09-01 19:12:20 +0200

    extend tailwind utils

--- utils/twind.ts
+++ utils/twind.ts
@@ -4,6 +4,13 @@ export * from "twind";
 export const config: Configuration = {
   preflight: true,
   darkMode: "class",
-  mode: "silent",
+  mode: "strict",
+  theme: {
+    borderWidth: {
+      sm: ".0625rem",
+      md: "0.125rem",
+      lg: "0.2rem",
+    },
+  },
 };
 if (IS_BROWSER) setup(config);
