bf19f407ff159689e387d2d5573c21eac7357058
hazn <aitakprojects@gmail.com>
2022-09-02 15:17:16 +0200

    make html more semantic

--- components/AboutMeSection/SubSection.tsx
+++ components/AboutMeSection/SubSection.tsx
@@ -12,8 +12,8 @@ interface SubSectionProps {
 export function SubSection({ heading, children }: SubSectionProps) {
   return (
     <div>
-      <div class={tw` border-b-sm border-black font-serif font-semibold`}>
-        {heading}
+      <div class={tw` border-b-sm border-black`}>
+        <h2 class={tw`font-serif font-semibold`}>{heading}</h2>
       </div>
       <div class={tw`pt-2`}>{children}</div>
     </div>
