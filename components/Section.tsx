7a2179dec6923d16b387504dc6131cdd774c007f
hazn <aitakprojects@gmail.com>
2022-09-02 19:06:17 +0200

    updated `Section.tsx` styling

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,4 +1,5 @@
 /** @jsx h */
+
 import { h } from "preact";
 import { tw } from "@twind";
 import type { VNode } from "preact";
@@ -12,12 +13,27 @@ export function Section({ heading, children }: SectionProps) {
   return (
     <section class={tw`pt-5`}>
       {heading != undefined ? (
-        <div>
-          <h1 class={tw`text-3xl font-serif font-semibold`}>{heading}</h1>
-          <div class={tw`border-b-md border-black -mx-4`}></div>
+        // <div>
+        //   <h1 class={tw`text-3xl font-serif font-semibold`}>{heading}</h1>
+        //   <div class={tw`border-b-md border-black -mx-4`}></div>
+        // </div>
+        <div class={tw`relative -mx-4`}>
+          <div
+            class={tw`absolute inset-0 flex items-center`}
+            aria-hidden="true"
+          >
+            <div class={tw`w-full border-b-md border-black`}></div>
+          </div>
+          <div class={tw`relative flex justify-center`}>
+            <h1
+              class={tw`px-3 bg-white text-3xl font-serif font-semibold`}
+              style="font-variant:small-caps;"
+            >
+              {heading}
+            </h1>
+          </div>
         </div>
       ) : null}
-
       {children}
     </section>
   );
