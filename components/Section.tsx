688fa1669557e78a27d6ba58ff4e0f4dea594124
hazn <aitakprojects@gmail.com>
2022-09-02 08:43:58 +0200

    Update styling

--- components/Section.tsx
+++ components/Section.tsx
@@ -10,7 +10,7 @@ interface SectionProps {
 
 export function Section({ heading, children }: SectionProps) {
   return (
-    <section>
+    <section class={tw`py-5`}>
       {heading != undefined ? (
         <div>
           <h1 class={tw`text-3xl font-serif font-semibold`}>{heading}</h1>
