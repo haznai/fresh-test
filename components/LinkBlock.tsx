baf9943e1bea20e1a2cea19752dea2b37a6294d1
hazn <aitakprojects@gmail.com>
2022-09-16 12:37:51 +0200

    start working on `LinkBlock.tsx`

---
+++ components/LinkBlock.tsx
@@ -0,0 +1,25 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+
+interface LinkBlockProps {
+  text: string;
+  href: string;
+}
+export function LinkBlock({ text, href }: LinkBlockProps) {
+  return (
+    <div
+      class={tw`relative inline-flex gap-0.5 pl-1 pr-1.5 py-0.5 overflow-hidden items-center group border-sm border-black`}
+    >
+      <span
+        class={tw`absolute left-0 w-6 h-12 duration-1000 -translate-x-10 rotate-[20deg] group-hover:translate-x-36 bg-black`}
+      />
+      <img
+        class={tw`h-4`}
+        src={"/iconblocks/link.svg"}
+        alt="logo of the technology"
+      />
+      <span class={tw` font-bold uppercase text-xs`}>{text}</span>
+    </div>
+  );
+}
